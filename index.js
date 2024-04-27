e301447b2a1d33da92b47b398968f5ca5dd38c5c
Amel TRIKI <amel_triki@yahoo.fr>
2024-04-27 19:13:08 +0200

    Sauvegarde 1

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port = process.env.PORT || 8080;
+
+// Middleware pour empêcher la suppression et la modification
+server.use((req, res, next) => {
+  if (req.method === "DELETE" || req.method === "PUT" || req.method === "PATCH") {
+    // Si la méthode de requête est DELETE, PUT ou PATCH, renvoyer une erreur
+    return res.status(403).json({ message: "Opération interdite" });
+  }
+  // Si la méthode de requête n'est pas DELETE, PUT ou PATCH, passer au middleware suivant
+  next();
+});
 
 server.use(middlewares);
 server.use(router);
